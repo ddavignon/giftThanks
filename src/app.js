3872c85ffa0872a38bdc7b159d538c0796a4ede3
dsignmind <dsignmind@gmail.com>
2017-05-25 13:42:59 -0700

    add working footer

--- src/app.js
+++ src/app.js
@@ -44,6 +44,7 @@ class App extends Component {
                         {/*<GetItemsList />*/}
                     <ItemList />
                     </ScrollView>
+                    <FooterBar />
                 </View>
             </Provider>
         );
--- src/components/FooterBar.js
+++ src/components/FooterBar.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import { Container, Footer, Button, Icon } from 'native-base';
-import { View } from 'react-native';
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 class FooterBar extends Component {
     render() {
         return (
-            <Container>
-                <Footer>
-                    <Button>
-                        <Icon size={30} color={'#fff'} name={'ion-pricetags'} />
-                    </Button>
-                    <Button>
-                        <Icon size={30} color={'#fff'} name={'ion-person-stalker'} />
-                    </Button>
-                    <Button>
-                        <Icon size={30} color={'#fff'} name={'ion-social-usd'} />
-                    </Button>
-                </Footer>
-            </Container>
+            <BottomNavigation
+                labelColor="white"
+                rippleColor="white"
+                style={styles.footerStyle}
+                //onTabChange={(newTabIndex) => alert(`New Tab at position ${newTabIndex}`)}
+            >
+            <Tab
+                barBackgroundColor="#37474F"
+                label="Contacts"
+                icon={<Icon size={24} color="white" name="people" />}
+            />
+            <Tab
+                barBackgroundColor="#4f6b99"
+                label="Events"
+                icon={<Icon size={24} color="white" name="view-list" />}
+            />
+            <Tab
+                barBackgroundColor="#00796B"
+                label="Tokens"
+                icon={<Icon size={24} color="white" name="monetization-on" />}
+            />
+          </BottomNavigation>
         );
     }
 }
+
+const styles = {
+    footerStyle: {
+        height: 56,
+        elevation: 8,
+        position: 'absolute',
+        left: 0,
+        bottom: -74,
+        right: 0
+    }
+};
 
 export default FooterBar;
