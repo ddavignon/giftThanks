6a3881c766f9d89ad8cdf4bac7aa266cb18c3e27
dsignmind <dsignmind@gmail.com>
2017-08-22 16:14:01 -0700

    fix get contacts, add/send item button

--- src/components/SendItemForm.js
+++ src/components/SendItemForm.js
@@ -13,7 +13,8 @@ class SendItemForm extends Component {
 
     state = {
         isFromText: '',
-        avatarSource: null
+        avatarSource: null,
+        emailText: ''
     }
 
     componentDidMount() {
@@ -30,11 +31,17 @@ class SendItemForm extends Component {
         .then((contact) => {
             // Replace this code
             console.log(contact);
+            this.setState({ emailText: contact.email });
         })
         .catch((error) => {
             console.log("ERROR CODE: ", error.code);
             console.log("ERROR MESSAGE: ", error.message);
         });
+    }
+
+    validateEmail(email) {
+      const emailRe = /([\w.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/igm;
+      return emailRe.test(email);
     }
 
     render() {
@@ -43,35 +50,41 @@ class SendItemForm extends Component {
         return (
             <View style={{ flex: 1, paddingTop: 70, }}>
                 <CardSection>
-                    <View style={{ flex: 1 }} >
-                        <TouchableOpacity
-                            style={container}
-                            onPress={() => {}}
-                        >
-                            <View style={[container, clothingItem, clothingItemContainer]}>
-                                { this.state.avatarSource === null
-                                    ? <Text>Select a Photo</Text>
-                                    : <Image
-                                        style={styles.clothingItem}
-                                        source={this.state.avatarSource}
-                                    />
-                                }
-                            </View>
-                        </TouchableOpacity>
+                    <View style={[container, clothingItem, clothingItemContainer]}>
+                        { this.state.avatarSource === null
+                            ? <Text>Select a Photo</Text>
+                            : <Image
+                                style={styles.clothingItem}
+                                source={this.state.avatarSource}
+                            />
+                        }
+                        <Input
+                            placeholder="Bob"
+                            label="From"
+                            value={this.state.isFromText}
+                            onChangeText={isFromText => this.setState({ isFromText })}
+                        />
                     </View>
                 </CardSection>
                 <CardSection>
                     <Input
-                        placeholder="Bob"
-                        label="From"
-                        value={this.state.isFromText}
-                        onChangeText={isFromText => this.setState({ isFromText })}
+                        placeholder="Mom@mail.com"
+                        label="Email"
+                        value={this.state.emailText}
+                        onChangeText={emailText => this.setState({ emailText })}
                     />
                 </CardSection>
                 <CardSection>
-                    <Button onPress={this.onButtonPressed}>
-                        Send Item
-                    </Button>
+                    {
+                        this.validateEmail(this.state.emailText) ?
+                        <Button onPress={() => {}}>
+                            Send Item
+                        </Button> :
+                        <Button onPress={this.onButtonPressed.bind(this)}>
+                            Get email from contacts
+                        </Button>
+                    }
+
                 </CardSection>
             </View>
         );
@@ -80,20 +93,21 @@ class SendItemForm extends Component {
 
 const styles = {
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
   clothingItemContainer: {
     borderColor: '#9B9B9B',
     borderWidth: 1 / PixelRatio.get(),
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center'
   },
   clothingItem: {
     borderRadius: 5,
-    width: 275,
-    height: 275
+    width: 75,
+    height: 75
   }
 };
 
