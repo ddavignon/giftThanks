47c6bbd31e9043233a55f0af5fccb80b91e7f656
dsignmind <dsignmind@gmail.com>
2017-08-22 20:51:11 -0700

    add text area, return to event page after email completion.

--- src/components/SendItemForm.js
+++ src/components/SendItemForm.js
@@ -4,10 +4,12 @@ import {
     Image,
     View,
     Text,
+    TextInput,
     PixelRatio
 } from 'react-native';
 import ContactsWrapper from 'react-native-contacts-wrapper';
 import Communications from 'react-native-communications';
+import { Actions } from 'react-native-router-flux';
 import { CardSection, Button, Input } from './common';
 
 class SendItemForm extends Component {
@@ -15,7 +17,10 @@ class SendItemForm extends Component {
     state = {
         isFromText: '',
         avatarSource: null,
-        emailText: ''
+        emailContactText: '',
+        emailBodyText: '',
+        emailSubjectText: '',
+        eventId: ''
     }
 
     componentDidMount() {
@@ -23,7 +28,8 @@ class SendItemForm extends Component {
         const { URL, name } = this.props.eventItem;
         this.setState({
             avatarSource: { uri: URL },
-            isFromText: name
+            isFromText: name,
+            emailBodyText: `${name},\n\n`
         });
     }
 
@@ -32,7 +38,7 @@ class SendItemForm extends Component {
         .then((contact) => {
             // Replace this code
             console.log(contact);
-            this.setState({ emailText: contact.email });
+            this.setState({ emailContactText: contact.email });
         })
         .catch((error) => {
             console.log("ERROR CODE: ", error.code);
@@ -41,7 +47,14 @@ class SendItemForm extends Component {
     }
 
     onSendButtonPressed() {
-        Communications.email([this.state.emailText], null, null, 'thank you !', 'Thanks for using Gift Thanks!');
+        const emailSignature = '\n\n\nThanks for using Gift Thanks!';
+        const { eventId } = this.props;
+        Communications.email(
+            [this.state.emailContactText],
+            null, null, 'thank you !',
+            `  ${this.state.emailBodyText} ${emailSignature}`
+        );
+        Actions.gifts({ eventId, type: 'back' });
     }
 
     validateEmail(email) {
@@ -50,12 +63,12 @@ class SendItemForm extends Component {
     }
 
     render() {
-        const { container, clothingItem, clothingItemContainer } = styles;
+        const { container, clothingItem, clothingItemContainer, textArea } = styles;
 
         return (
             <View style={{ flex: 1, paddingTop: 70, }}>
                 <CardSection>
-                    <View style={[container, clothingItem, clothingItemContainer]}>
+                    <View style={[clothingItem, clothingItemContainer]}>
                         { this.state.avatarSource === null
                             ? <Text>Select a Photo</Text>
                             : <Image
@@ -63,25 +76,31 @@ class SendItemForm extends Component {
                                 source={this.state.avatarSource}
                             />
                         }
-                        <Input
-                            placeholder="Bob"
-                            label="From"
-                            value={this.state.isFromText}
-                            onChangeText={isFromText => this.setState({ isFromText })}
-                        />
                     </View>
                 </CardSection>
                 <CardSection>
                     <Input
                         placeholder="Mom@mail.com"
                         label="Email"
-                        value={this.state.emailText}
-                        onChangeText={emailText => this.setState({ emailText })}
+                        value={this.state.emailContactText}
+                        onChangeText={emailContactText => this.setState({ emailContactText })}
                     />
                 </CardSection>
+                <View>
+                    <TextInput
+                        style={textArea}
+                        multiline={true}
+                        placeholder={'Mom@mail.com'}
+                        editable={true}
+                        maxLength={200}
+                        numberOfLines={6}
+                        onChangeText={(emailBodyText) => this.setState({ emailBodyText })}
+                        value={this.state.emailBodyText}
+                    />
+                </View>
                 <CardSection>
                     {
-                        this.validateEmail(this.state.emailText) ?
+                        this.validateEmail(this.state.emailContactText) ?
                         <Button onPress={this.onSendButtonPressed.bind(this)}>
                             Send Item
                         </Button> :
@@ -104,6 +123,7 @@ const styles = {
     backgroundColor: '#F5FCFF'
   },
   clothingItemContainer: {
+    flex: 1,
     borderColor: '#9B9B9B',
     borderWidth: 1 / PixelRatio.get(),
     justifyContent: 'space-between',
@@ -111,9 +131,15 @@ const styles = {
   },
   clothingItem: {
     borderRadius: 5,
-    width: 75,
-    height: 75
-  }
+    width: 80,
+    height: 80
+},
+textArea: {
+    paddingLeft: 5,
+    height: 100,
+    fontSize: 18
+}
+
 };
 
 export default SendItemForm;
