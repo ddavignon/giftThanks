e862e1699f596127f65e41f19d7e2f78d7ad82be
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-04 18:38:51 -0700

    connect google token to firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -43,11 +43,55 @@ class LoginForm extends Component {
             .catch(err => alert(err));
     }
 
+    onSignIn(googleUser) {
+        console.log('Google Auth Response', googleUser);
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        const unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
+          unsubscribe();
+          // Check if we are already signed-in Firebase with the correct user.
+          if (!this.isUserEqual(googleUser, firebaseUser)) {
+            // Build Firebase credential with the Google ID token.
+            const credential = firebase.auth.GoogleAuthProvider.credential(
+                googleUser.idToken);
+            // Sign in with credential from the Google user.
+            firebase.auth().signInWithCredential(credential)
+                .then(user => Actions.tabbar())
+                .catch(error => {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // The email of the user's account used.
+                    const email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    const credential = error.credential;
+                    // ...
+                });
+          } else {
+            console.log('User already signed-in Firebase.');
+          }
+        });
+      }
+
+      isUserEqual(googleUser, firebaseUser) {
+        if (firebaseUser) {
+          const providerData = firebaseUser.providerData;
+          for (let i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.getBasicProfile().getId()) {
+              // We don't need to reauth the Firebase connection.
+              return true;
+            }
+          }
+        }
+        return false;
+      }
+
     _signIn() {
       GoogleSignin.currentUserAsync({
         iosClientId: '1097148081266-ls3e9l7eqtf10456as58l4gid60pl8cs.apps.googleusercontent.com',
       }).then((user) => {
         console.log('USER: ', user);
+        this.onSignIn(user);
         this.setState({ googleUser: user });
       }).done();
       
